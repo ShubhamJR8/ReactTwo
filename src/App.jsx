53bcfdbce66bded29ef2b56d3fb082e4169eb84c
ShubhamJR8 <shubjr1998@gmail.com>
2024-05-31 03:02:31 +0530

    reactTwo Complete SPA

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
+import Video from './components/Video';
+import Upload from './components/Upload';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/videos' element={<Video />} />
+        <Route path='/upload' element={<Upload />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
       </Routes>
       <Footer />
     </Router>
